a931481ec383096a53fce1262528b7f9885ed1da
Leoric01 <troglodit.nick@seznam.cz>
2025-08-07 16:03:39 +0200

    ingredient fixed

--- src/main/java/leoric/pizzacipollastorage/controllers/IngredientController.java
+++ src/main/java/leoric/pizzacipollastorage/controllers/IngredientController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/ingredients")
+@RequestMapping("/api/ingredient")
 @RequiredArgsConstructor
 public class IngredientController {
     private final IngredientService ingredientService;
